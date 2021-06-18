aa929c103c7f788e0adc37273b78779b31bf0502
Denis <denmagg.work@gmail.com>
2021-06-18 21:34:36 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Denis Medvedev on 18/06/2021.
+//  Copyright © 2021 Denis Medvedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
